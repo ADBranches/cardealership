64a49386fea93a2f92e35ec7e766ced5ff51d716
Edwin Kambale <edwin@postbabyblissfoundation.onmicrosoft.com>
2026-07-31 16:52:34 +0300

    Add global authentication bootstrap state

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,23 @@
-﻿import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {LoginPage} from "../pages/Login/LoginPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/auth";
+import { LoginPage } from "../pages/Login/LoginPage";
 import RegisterPage from "../pages/Register";
 import Admin from "../pages/Admin";
 import TestTasks from "../pages/TestTasks/TestTasks";
-import {HomePage} from "../pages/Home/HomePage";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import Home from "../pages/Home";
+import { HomePage } from "../pages/Home/HomePage";
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-
-<Route path="/test" element={<TestTasks />} />
-
-<Route path="/login" element={<LoginPage />} />
-
-<Route path="/register" element={<RegisterPage />} />
-
-<Route path="/Admin" element={<Admin />} />
-
-<Route path="/*" element={<HomePage />} />
-
-</Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/test" element={<TestTasks />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/Admin" element={<Admin />} />
+          <Route path="/*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
