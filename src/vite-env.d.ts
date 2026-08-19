eab7565d60b6f93bae614cbf817c1ea918fbfbac
Edwin Kambale <edwin@postbabyblissfoundation.onmicrosoft.com>
2026-08-19 15:11:44 +0300

    Add booking availability service and hook

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
   readonly VITE_PROFILE_MOCK_MODE?: string;
+  readonly VITE_AVAILABILITY_MOCK_MODE?: string;
 }
 
 interface ImportMeta {
